7be8a6efd131c29b05747f9748e5937d7ae8ab66
ay_vicky <ayvicky1@gmail.com>
2020-10-26 04:25:48 -0700

    setup static file serve for page not found 404!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -16,7 +17,7 @@ app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1> Page not found! </h1>')
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
