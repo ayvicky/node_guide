45917c3bac45638df08d1d65ca30e82ab71520a7
ay_vicky <ayvicky1@gmail.com>
2020-10-26 03:03:46 -0700

    parse request body - add body-parser middleware

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+// app.use(bodyParser.json());
+
+app.use('/', (req, res, next) => {
+    console.log('always run');
     next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in the product middleware');
+    res.send(`
+        <form action='/product' method='POST'>
+            <input type='text' name='title'>
+            <button type='submit'> Add Product </button>
+        </form>
+    `)
 });
 
-app.use((req, res, next) => {
-    console.log('in anothe middleware');
-    res.send('<h1> Hello world! </h1>')
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 });
-
+app.use('/', (req, res, next) => {
+    console.log('always run!');
+    res.send('<h1> Hello from express! </h1>')
+})
 
 // const server = http.createServer(app);
 
