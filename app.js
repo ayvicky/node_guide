257af4cbb6f1d4fa3c8dc4f0b2ad951429429f10
ay_vicky <ayvicky1@gmail.com>
2020-10-26 04:21:19 -0700

    setup static html files and send in response to the user

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -16,32 +15,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
-app.use('/', (req, res, next) => {
-    console.log('always run');
-    next();
-})
-
-app.get('/add-product', (req, res, next) => {
-    console.log('in the product middleware');
-    res.send(`
-        <form action='/product' method='POST'>
-            <input type='text' name='title'>
-            <button type='submit'> Add Product </button>
-        </form>
-    `)
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-});
-
-
 app.use((req, res, next) => {
     res.status(404).send('<h1> Page not found! </h1>')
 })
 
-// const server = http.createServer(app);
-
-// server.listen(3000);
 app.listen(3000);
