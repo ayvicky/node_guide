c4d3458e864c41a800e6870222d3445138998aa7
ay_vicky <ayvicky1@gmail.com>
2020-10-27 04:42:55 -0700

    templating engines done

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('handlebars', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'handlebars',
-}));
 // app.set('view engine', 'pug');
-app.set('view engine', 'handlebars');
+
+    // app.engine('handlebars', expressHbs({
+    //     layoutsDir: 'views/layouts/',
+    //     defaultLayout: 'main-layout',
+    //     extname: 'handlebars',
+    // }));
+// app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // parse application/x-www-form-urlencoded
@@ -30,7 +32,9 @@ app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
     // res.sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found!'
+    });
 })
 
 app.listen(3000);
