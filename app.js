3c86a4195e37cb03ccb76feff8a9dde44204d0f4
ay_vicky <ayvicky1@gmail.com>
2020-10-27 05:34:13 -0700

    setup for mvc

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-// app.set('view engine', 'pug');
-
-    // app.engine('handlebars', expressHbs({
-    //     layoutsDir: 'views/layouts/',
-    //     defaultLayout: 'main-layout',
-    //     extname: 'handlebars',
-    // }));
-// app.set('view engine', 'handlebars');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,7 +22,6 @@ app.use('/admin', adminRoutes.router);
 app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
-    // res.sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {
         pageTitle: 'Page Not Found!'
     });
