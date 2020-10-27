a3049e8daebf21d1d26123d0fe4ee4571796691a
ay_vicky <ayvicky1@gmail.com>
2020-10-27 09:10:58 -0700

    added new views

--- models/product.js
+++ models/product.js
@@ -1,13 +1,38 @@
-const products = [];
+const path = require('path');
+const fs = require('fs');
+
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+        fs.readFile(p, (err, fileContent) => {})
+        // products.push(this);
     }
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);      
+        // return products;
     }
-}
+};
