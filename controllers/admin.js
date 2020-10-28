c139151e8171a504bb3236bf5b676ab135f5df6d
ay_vicky <ayvicky1@gmail.com>
2020-10-28 06:06:11 -0700

    cart items displayed

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,20 +1,65 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
         productCSS: true,
-        activeAddProduct: true
+        activeAddProduct: true,
+        editing: false
     });
 };
 
 exports.postAddProduct = (req, res, next) => {
     const {title, imageUrl, description, price} = req.body;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    })
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const {
+        productId,
+        title,
+        imageUrl,
+        description,
+        price
+    } = req.body;
+    const updatedProduct = new Product(
+        productId,
+        title,
+        imageUrl,
+        description,
+        price
+    );
+    updatedProduct.save();
+    res.redirect('/');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    const { productId } = req.body;
+    Product.deleteById(productId);
+    res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) => {
--- models/cart.js
+++ models/cart.js
@@ -33,12 +33,39 @@ static addProduct(id, productPrice) {
             updatedProduct = {id: id, qty: 1};
             cart.products = [...cart.products, updatedProduct];
         }
-        console.log(cart);
         console.log(productPrice);
         cart.totalPrice = cart.totalPrice + +productPrice;
+        console.log(cart);
         fs.writeFile(p, JSON.stringify(cart), err => {
             console.log(err);
         });
     });
 }
+static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return;
+        }
+        const updatedCart = {...JSON.parse(fileContent)};
+        const product = updatedCart.products.find(prod => prod.id === id);
+        const productQty = product.qty;
+        updatedCart.products = updatedCart.products.filter(
+            prod => prod.id !== id
+        );
+        updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+        fs.writeFile(p, JSON.stringify(updatedCart), err => {
+            console.log(err);
+        });
+    });
 }
+static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+        const cart = JSON.parse(fileContent);
+        if(err) {
+            cb(null);
+        } else {
+            cb(cart);
+        }
+    });
+}
+};
