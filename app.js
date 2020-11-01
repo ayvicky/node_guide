0559f49d4f93e8e0e7b189851c84eda918879a1f
ay_vicky <ayvicky1@gmail.com>
2020-11-01 08:33:22 -0800

    setup creation of new user & add user to request using middleware

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findAll({where: {id: 1}})
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,9 +40,19 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
-sequalize.sync({ force: true }).then(result => {
-console.log(result);
-app.listen(3000);
-}).catch(err => {
+sequalize.sync().then(result => {
+    return User.findAll({where: {id: 1}});
+})
+.then(user => {
+    if(!user) {
+        return User.create({ name: 'Max', email: 'test@test.com'});
+    }
+    return user;
+})
+.then(user => {
+    console.log(user);
+    app.listen(3000);
+})
+.catch(err => {
     console.log(err);
 });
