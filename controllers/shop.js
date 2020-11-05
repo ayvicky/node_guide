d0bedeb1ef864cc87e76c834a422e5b78e568f9b
ay_vicky <ayvicky1@gmail.com>
2020-11-05 12:22:45 -0800

    added add order functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -82,27 +82,8 @@ exports.getOrders = (req, res, next) => {
         }).catch(err => console.log(err));
 };
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-        .getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts();
-        })
-        .then(products => {
-            return req.user
-                        .createOrder()
-                        .then(order => {
-                            return order.addProducts(products.map(product => {
-                                product.orderItem = { quantity: product.cartItem.quantity };
-                                return product;
-                            }));
-                        })
-                        .catch(err => console.log(err));
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null);
-        })
+        .addOrder()
         .then(result => {
             res.redirect('/orders');
         })
