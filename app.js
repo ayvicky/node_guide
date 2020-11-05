c1bf008cccfb669d711411804f1b4e608ec60c37
ay_vicky <ayvicky1@gmail.com>
2020-11-05 10:52:40 -0800

    setup user model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./utils/database').mongoConnect;
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -20,6 +21,16 @@ app.use(bodyParser.urlencoded({extended: false}))
 // app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('5fa448ea543a2014386f7657')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
