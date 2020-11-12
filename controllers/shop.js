8bbdc3f392dd870e75aeb0b316f72d9662f5a33f
ay_vicky <ayvicky1@gmail.com>
2020-11-12 12:24:00 -0800

    add functionality for delete item from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -67,7 +67,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDelete = (req, res, next) => {
     const prodId = req.body.productId;
     req.user
-        .deleteItemFromCart(prodId)
+        .removeFromCart(prodId)
         .then(result => {
             res.redirect('/cart');
         })
