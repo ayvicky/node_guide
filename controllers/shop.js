899f3b68a5f44ef19dd624707d3bad326c9dc704
ay_vicky <ayvicky1@gmail.com>
2020-11-14 01:55:13 -0800

    fix order button issue

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,6 +53,7 @@ exports.getCart = (req, res, next) => {
         .catch(err => console.log(err));
 };
 exports.postCart = (req, res, next) => {
+    console.log(req.user);
     const prodId = req.body.productId;
     Product.findById(prodId)
         .then(product => {
@@ -93,7 +94,7 @@ exports.postOrder = (req, res, next) => {
             });
             const order = new Order({
                 user: {
-                    name: req.user.name,
+                    email: req.user.email,
                     userId: req.user
                 },
                 products: products
