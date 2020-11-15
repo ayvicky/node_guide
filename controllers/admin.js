381d82d308f5705b6dd6a1ff576476e532173ce9
ay_vicky <ayvicky1@gmail.com>
2020-11-15 00:00:59 -0800

    added authorization only fetch current user products in admin area

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,7 +84,7 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({userId: req.user._id})
         .select('title price -_id')
         .populate('userId', 'name')
         .then(products => {
